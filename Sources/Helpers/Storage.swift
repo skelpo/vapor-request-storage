aeaf4f41d7ffd2379422d558578e12143a6b79ef
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-17 08:05:37 -0500

    Documented Storage service class

--- Sources/Helpers/Storage.swift
+++ Sources/Helpers/Storage.swift
@@ -1,7 +1,14 @@
 import Service
 
+/// The base type used to store arbitrary data
+/// in a single request.
+/// This type works with a request's `privateContainer`
+/// to hold the data.
 public class Storage: Service {
+    
+    /// The underlying dictionary that holds everything.
     public var cache: [String: Any] = [:]
     
+    ///
     public init() {}
 }
