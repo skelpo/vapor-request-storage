156a4fccd34901cdf94239e1481b6cf357531f31
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-17 08:15:28 -0500

    Documented StorageProvider class

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -1,17 +1,26 @@
 import Vapor
 
-public class Provider: Vapor.Provider {
+/// Used to register the `Storage` service with a `Services` instance.
+///
+///     try service.register(StorageProvider())
+public class StorageProvider: Vapor.Provider {
+    
+    /// See `Provider.repositoryName`.
     public static var repositoryName: String = "VaporRequestStorage"
     
+    /// We add this because the synthesized initializer is defined as `private`.
     public init () {}
     
+    /// See `Provider.register`.
     public func register(_ services: inout Services) throws {
         services.register() { (container) -> (Storage) in
             return Storage()
         }
     }
     
+    /// See `Provider.boot`.
     public func boot(_ worker: Container) throws {}
     
+    /// See `Provider.didBoot`.
     public func didBoot(_ worker: Container) throws -> EventLoopFuture<Void> { return Future.map(on: worker) {()} }
 }
